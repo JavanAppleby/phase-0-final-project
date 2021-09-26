fd5ccb425bca298876a7ff55acc1f850bbb03045
JavanAppleby <styx_n_stonz@hotmail.com>
2021-09-26 17:52:27 +1000

    Added d20 Roll to script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function d20(min, max) {
+    let minNum = min;
+    let maxNum = max;
+    let mathLogic = Math.floor(Math.random() * (max - min + 1)) + min;
+    return mathLogic
+}
+
+function rollInit(minimum, maximum) {
+    let theRoll = d20(minimum, maximum);
+    
+    if (theRoll === 1) {
+        var printedStatement = `You might want to try again, because that ${theRoll} was a CRITICAL FAIL!`
+    } else if (theRoll === 20) {
+        var printedStatement = `Whoa, we got a high roller here! That ${theRoll} was a CRITICAL SUCCESS!`
+    } else {
+        var printedStatement = `You rolled a ${theRoll}. That's good enough! You may pass` 
+    };
+    return printedStatement;
+}
+
+console.log(rollInit(1, 20))
